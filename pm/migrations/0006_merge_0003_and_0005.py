09bf6451e8821c77c63debdeae407851dce82d56
Gilbert <gmutai1194@gmail.com>
2026-03-03 16:53:59 +0300

    Migrations merged

---
+++ pm/migrations/0006_merge_0003_and_0005.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("pm", "0003_remove_project_provisioning_form_and_more"),
+        ("pm", "0005_remove_unique_client_project_title_ci"),
+    ]
+
+    operations = []
